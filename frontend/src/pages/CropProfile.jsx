d005b027390710539a9340c8dcbd8309ed433449
name <123867179+agnijeetsharma@users.noreply.github.com>
2024-09-17 22:09:13 +0530

    updated CropProfile component to display crop details and send message functionality

--- frontend/src/pages/CropProfile.jsx
+++ frontend/src/pages/CropProfile.jsx
@@ -4,19 +4,32 @@ import { useSelector } from "react-redux";
 
 
 const CropProfile = () => {
+    const token=useSelector(store=>store.user.accessToken)
+    // console.log(token);
     const [message ,setmessage]=useState(false)
+    const [content,setContent]=useState("");
     const selector=useSelector(store=>store.crop);
-    console.log(selector);
+    const {description,title,cropImage,like,updatedAt,createdAt,farmerId}=selector.crop
+    // console.log(selector.crop);
     const handleMessageClick=async(e)=>{
         e.preventDefault();
         setmessage(!message);
-        // useEffect(()=>{
-        //     const response=await axios.post("http://localhost:3000/api/v1/users/dendMessage",{
-        //         "content":message,
-        //        " receiverId":,
-        //        " senderId":,
-        //     }) 
-        // // })
+       
+            const response=await axios.post("http://localhost:3000/api/v1/users/sendMessage",
+              {  "content":content,
+               "receiverId":farmerId,
+              },
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${token}`, 
+                },
+              }
+              
+            
+        ) 
+            console.log(response);
+       
 
     }
   return (
@@ -25,9 +38,12 @@ const CropProfile = () => {
  
       <div className="crop-details mb-8">
         <h1 className="text-3xl font-semibold text-green-700">{"Crop Details"}</h1>
-        <p className="text-lg text-gray-600">Type: {}</p>
+        <p className="text-lg text-gray-600">Type: {title}</p>
         {/* <p className="text-lg text-gray-600">Price: ${crop.price} per unit</p> */}
-        <p className="text-lg text-gray-600">Description: {}</p>
+        <p className="text-lg text-gray-600">Description: {description}</p>
+        <p className="text-lg text-gray-600">Like: {like.length}</p>
+        <p className="text-lg text-gray-600">CreatedAt: {createdAt}</p>
+        <p className="text-lg text-gray-600">UpdatedAt: {updatedAt}</p>
       </div>
 
      
@@ -49,7 +65,7 @@ const CropProfile = () => {
      { message&&<div className="text-center">
            <div className="">
               <label />
-              <input type="text" placeholder="enter your message" className="w-full p-4 border border-gray-200 rounded"/>
+              <input type="text" value={content} onChange={e=>setContent(e.target.value)} placeholder="enter your message" className="w-full p-4 border border-gray-200 rounded"/>
            </div>
       </div>}
   </div>
